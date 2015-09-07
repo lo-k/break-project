6e9a9dd7b74581742d80d65b8b96c8f3e94bfdd8
Kari <kari.lonczak@gmail.com>
2015-09-07 17:36:43 +0100

    Added placeholders for basic CRUD methods

--- app/controllers/city_lists_controller.rb
+++ app/controllers/city_lists_controller.rb
@@ -1,2 +1,18 @@
 class CityListsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def update
+  end
+
 end
--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -1,2 +1,15 @@
 class ListItemsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
 end
