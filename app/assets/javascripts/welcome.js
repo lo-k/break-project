8737e8220d8a5888b7f96d5e6c4d6db8f5b855d7
Kari <kari.lonczak@gmail.com>
2015-09-07 17:27:38 +0100

    Clicking home page button makes the modal appear to then login with.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,28 +1,22 @@
+$(function() {
+  // ************************************************************************************
+  //                          Home Page
+  // Click "Find Your List" > 
+  // adds "display:none to the button"
+  // removes "display: none" from modal box; opens modal with user name and password
 
-// ************************************************************************************
-//                          Home Page
-// Click "Find Your List" > 
-// adds "display:none to the button"
-// removes "display: none" from modal box; opens modal with user name and password
+  $('.open-login-button').on('click', function() {
+    $(this).addClass('hide');
+    $('.modalcontainer').removeClass('hide');
+  });
 
-$('.find-list-button').on('click', function() {
-  $(this).addClass('hide');
-  $('.modalcontainer').removeClass('hide');
-});
+  // loading icon
 
-// Takes the name that you enter in the Login box and puts it into the account page title?
+  // Wait for window load
+  // $(window).load(function() {
+  //   // Animate loader off screen
+  //   $('.spinner').fadeOut('slow');
+  //   $('.loading-screen').fadeOut('slow');
+  // });
 
-// $('.save-button').on('click',function() {
-//  $('#name').val().prepend('.your-name');
-//  console.log('.your-name');
-// });
-
-// loading icon
-
-// Wait for window load
-$(window).load(function() {
-  // Animate loader off screen
-  $('.spinner').fadeOut('slow');
-  $('.loading-screen').fadeOut('slow');
-});
-
+})
