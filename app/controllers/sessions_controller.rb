98c2a49ac75ebf6159abd6ea6e8e727f7baac668
Kari <kari.lonczak@gmail.com>
2015-09-08 10:25:53 +0100

    Added create and destroy methods to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,15 @@
 class SessionsController < ApplicationController
 
-  def new
-  end
-
   def create
+    @user = User.find_by(email: params[:session][:email])
+
+    redirect_to dashboard_path
   end
 
   def destroy
+    session[:user_id] = nil
+    
+    redirect_to root_path
   end
 
 end
