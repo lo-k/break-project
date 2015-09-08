3046d967c067fac9e9943724cb816479854c40ca
Kari <kari.lonczak@gmail.com>
2015-09-08 10:32:08 +0100

    Wrote post#create spec for users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+  describe "POST #create" do
+    it "creates a new user" do
+      get :create, {user: {name: "Joe", email: "joe@email.com", password: "hello", password_confirmation: "hello"}}
+
+      expect(User.all.count).to eq 1
+    end
+  end
+
 end
