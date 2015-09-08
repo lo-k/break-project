84129b03032050710cdc0aec515743e8c9f115d0
Kari <kari.lonczak@gmail.com>
2015-09-08 11:07:33 +0100

    Added line to set the session[:user_id] when you signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:session][:email])
+    session[:user_id] = @user.id
 
     redirect_to dashboard_path
   end
