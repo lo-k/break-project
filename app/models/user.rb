6024d166ab0e9285ee6976071b4b966eefa3ac1a
Kari <kari.lonczak@gmail.com>
2015-09-10 17:04:14 +0100

    Added validations to user model and wrote specs to test validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
   # Validations
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: {with: /@/}
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 
   # Associations
   has_secure_password
   has_many :city_lists
-  has_many :list_items
+  has_many :list_items, :through => :city_lists
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+    it "requires a name, email, password and password confirmaiton" do
+      user = User.new
+
+      expect(user).to_not be_valid
+      expect(user.errors.keys).to include(:name, :email, :password, :password_confirmation)
+    end
+
+    it "has many city_lists" do
+      expect(User.reflect_on_association(:city_lists).macro).to eq(:has_many)
+    end
+
+    it "has many list_items" do
+      expect(User.reflect_on_association(:list_items).macro).to eq(:has_many)
+    end
+
+  end
 end
