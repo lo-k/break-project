9848640951e5d685587b9a12882d86573ef83c5d
Kari <kari.lonczak@gmail.com>
2015-09-10 17:01:19 +0100

    Added validations for list_item model and wrote specs to test for validations and associations

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -1,5 +1,9 @@
 class ListItem < ActiveRecord::Base
   # Validations
+  validates :title, presence: true
+  # validates :complete, presence: true
+  # validates :favorite, presence: true
+  # validates :city_list_id, presence: true
 
   # Associations
   belongs_to :city_list
--- spec/models/list_item_spec.rb
+++ spec/models/list_item_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe ListItem, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+    # ADD MORE VALIDATIONS ONCE THEY ARE SOLIDIFIED
+    it "requires a title" do
+      list_item = ListItem.new
+
+      expect(list_item).to_not be_valid
+      expect(list_item.errors.keys).to include(:title)
+    end
+
+    it "belongs to a user" do
+      expect(ListItem.reflect_on_association(:user).macro).to eq(:belongs_to)
+    end
+
+    it "belongs to a city_list" do
+      expect(ListItem.reflect_on_association(:city_list).macro).to eq(:belongs_to)
+    end
+
+  end
 end
