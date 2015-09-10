8714419868dabdeed9509c62899169dc5a113e9a
Kari <kari.lonczak@gmail.com>
2015-09-10 18:31:52 +0100

    Added model methods to calc the current percent of list_items that are completed and to count the number of list_items that are favorited.

--- app/models/city_list.rb
+++ app/models/city_list.rb
@@ -6,4 +6,19 @@ class CityList < ActiveRecord::Base
   # Associations
   belongs_to :user
   has_many :list_items
+
+  # Scopes
+  def percent_completed
+    total = list_items.count
+    complete = list_items.where(complete: true).count
+
+    percent = ((complete.to_f / total.to_f) * 100).to_i
+
+    return percent
+  end
+
+  def count_favs
+    list_items.where(favorite: true).count
+  end
+  
 end
