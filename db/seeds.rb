d10d37436d4b313313d4b95d18b89de0ec39f50f
Kari <kari.lonczak@gmail.com>
2015-09-10 12:02:46 +0100

    Created seed data for users, city_lists and list_items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+CSV.foreach("db/seed_data_users.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+    User.create(row.to_hash)
+end
+
+CSV.foreach("db/seed_data_city_lists.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+    CityList.create(row.to_hash)
+end
+
+CSV.foreach("db/seed_data_list_items.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+    ListItem.create(row.to_hash)
+end
